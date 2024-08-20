3656991c234cf21ad585fb4cc28fa315fe5d9e9d
Juan Camilo Narváez <ulvenforst@gmail.com>
2024-08-19 20:58:28 -0500

    clase 2, LCS

---
+++ programacionDinamicaVoraz/clase2/leastCommonSubsequence/leastCommonSubsequence.go
@@ -0,0 +1,50 @@
+package leastCommonSubsequence
+
+import "fmt"
+
+func LCSLength(X, Y string, m, n int) ([][]int, [][]string) {
+    c := make([][]int, m+1)
+    b := make([][]string, m+1)
+    for i := range c {
+        c[i] = make([]int, n+1)
+        b[i] = make([]string, n+1)
+    }
+
+    for i := 1; i <= m; i++ {
+        c[i][0] = 0
+    }
+    for j := 0; j <= n; j++ {
+        c[0][j] = 0
+    }
+
+    for i := 1; i <= m; i++ {
+        for j := 1; j <= n; j++ {
+            if X[i-1] == Y[j-1] {
+                c[i][j] = c[i-1][j-1] + 1
+                b[i][j] = "↖"
+            } else if c[i-1][j] >= c[i][j-1] {
+                c[i][j] = c[i-1][j]
+                b[i][j] = "↑"
+            } else {
+                c[i][j] = c[i][j-1]
+                b[i][j] = "←"
+            }
+        }
+    }
+
+    return c, b
+}
+
+func PrintLCS(b [][]string, X string, i, j int) {
+    if i == 0 || j == 0 {
+        return
+    }
+    if b[i][j] == "↖" {
+        PrintLCS(b, X, i-1, j-1)
+        fmt.Print(string(X[i-1])) // same as Y[j-1]
+    } else if b[i][j] == "↑" {
+        PrintLCS(b, X, i-1, j)
+    } else {
+        PrintLCS(b, X, i, j-1)
+    }
+}
---
+++ programacionDinamicaVoraz/clase2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+  "github.com/Ulvenforst/ada2/programacionDinamicaVoraz/clase2/leastCommonSubsequence"
+)
+
+func main() {
+  X := "ABCBDAB"
+  Y := "BDCABA"
+  m := len(X)
+  n := len(Y)
+
+  c, b := leastCommonSubsequence.LCSLength(X, Y, m, n)
+
+  fmt.Printf("Length of LCS: %d\n", c[m][n])
+  fmt.Print("LCS: ")
+  leastCommonSubsequence.PrintLCS(b, X, m, n)
+  fmt.Println()
+}

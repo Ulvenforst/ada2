62142b07f7e98fbdf6bb687c6ade5c35d81383ca
Juan Camilo Narváez <ulvenforst@gmail.com>
2024-09-11 17:47:01 -0500

    Binomial developed

--- programacionDinamicaVoraz/clase1/binomialCoefficients/binomialCoefficients.go
+++ programacionDinamicaVoraz/clase1/binomialCoefficients/binomialCoefficients.go
@@ -1,19 +1,19 @@
 // Description: This package provides a function to calculate the binomial coefficient of n and k.
 package binomialCoefficients
 
-const MAXN = 100
-
 func BinomialCoefficient(n, k int) int64 {
-  var bc [MAXN+1][MAXN+1]int64
+  bc := make([][]int64, n+1)
+  for i := range bc {
+    bc[i] = make([]int64, n+1)
+  }
 
+  // Base cases
   for i := 0; i <= n; i++ {
     bc[i][0] = 1
+    bc[i][i] = 1
   }
 
-  for j := 0; j <= n; j++ {
-    bc[j][j] = 1
-  }
-
+  // Recurrence representation
   for i := 2; i <= n; i++ {
     for j := 1; j < i; j++ {
       bc[i][j] = bc[i-1][j-1] + bc[i-1][j]
--- programacionDinamicaVoraz/clase1/main.go
+++ programacionDinamicaVoraz/clase1/main.go
@@ -2,9 +2,11 @@
 package main
 
 import (
-  "fmt"
-  "time"
-  "github.com/Ulvenforst/ada2/programacionDinamicaVoraz/clase1/fibonacci"
+	"fmt"
+	"time"
+
+	"github.com/Ulvenforst/ada2/programacionDinamicaVoraz/clase1/binomialCoefficients"
+	"github.com/Ulvenforst/ada2/programacionDinamicaVoraz/clase1/fibonacci"
 )
 
 func main() {
@@ -13,6 +15,8 @@ func main() {
   // var fibNumber int64 = fibonacci.FibCDriver(50)
   // var fibNumber int64 = fibonacci.FibDp(50)
   var fibNumber int64 = fibonacci.FibUltimate(50)
+  var binomial int64 = binomialCoefficients.BinomialCoefficient(5,2)
   
   fmt.Println(fibNumber, "Time: ", time.Since(start))
+  fmt.Println("binomial: ", binomial)
 }
